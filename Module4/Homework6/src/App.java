05b7742858f345409ee7d9e8351c97a140e52cba
vbumazhenko <vbumazhenko@mail.ru>
2019-12-21 21:42:04 +0700

    Add Homework6

--- Module4/Homework6/src/App.java
+++ Module4/Homework6/src/App.java
@@ -1,18 +1,15 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 public class App {
     public static void main(String[] args) {
 
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EEEE");
 
-        // Установим в календарь дату рождения: 21.04.1984,
-        // учитывая, что месяцы тут начинаются с 0.
+        // Установим в календарь дату рождения: 21.04.1984.
         Calendar calendar = Calendar.getInstance();
-        calendar.set(1984, 3, 21);
+        calendar.set(1984, Calendar.APRIL, 21);
 
         int age = 0;
         while (calendar.getTime().getTime() < System.currentTimeMillis()) {
