e9051de6bf6951c1ea5ff571c366a5b05c1ee8ec
vbumazhenko <vbumazhenko@mail.ru>
2019-12-21 18:52:25 +0700

    Add Homework5_1

--- Module4/Homework5_1/src/App.java
+++ Module4/Homework5_1/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+        System.out.println(text);
+
+        int sum = 0;
+        String[] lines = text.split(",\\s");
+        for (String str : lines) {
+            sum += Integer.parseInt(str.replaceAll("[^0-9]", ""));
+        }
+
+        System.out.println("Сумма заработка составляет: " + sum);
 
     }
 }
