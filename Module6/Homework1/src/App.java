010ae5e7a331c6251c4d31c237d05c70b60e0284
vbumazhenko <vbumazhenko@mail.ru>
2020-01-07 11:34:22 +0700

    Добавлен вывод комиссии.

--- Module6/Homework1/src/App.java
+++ Module6/Homework1/src/App.java
@@ -7,27 +7,29 @@ public class App {
 
         // Основной счет
         Account myAcc = new Account(1000);
-        System.out.println(myAcc.getBalance());
+        System.out.println("Остаток: " + myAcc.getBalance());
         myAcc.putMoney(500);
-        System.out.println(myAcc.getBalance());
+        System.out.println("Остаток: " + myAcc.getBalance());
         myAcc.withdrawMoney(1400);
-        System.out.println(myAcc.getBalance());
+        System.out.println("Остаток: " + myAcc.getBalance());
 
         // Депозит
+        System.out.println();
         DepoAccount myDepoAcc = new DepoAccount(2000);
-        System.out.println(myDepoAcc.getBalance());
+        System.out.println("Остаток: " + myDepoAcc.getBalance());
         myDepoAcc.putMoney(500);
-        System.out.println(myDepoAcc.getBalance());
+        System.out.println("Остаток: " + myDepoAcc.getBalance());
         myDepoAcc.withdrawMoney(2400);
-        System.out.println(myDepoAcc.getBalance());
+        System.out.println("Остаток: " + myDepoAcc.getBalance());
 
         // Карта
+        System.out.println();
         CardAccount myCardAcc = new CardAccount(100);
-        System.out.println(myCardAcc.getBalance());
+        System.out.println("Комиссия при снятии: " + CardAccount.getFee() + "%");
+        System.out.println("Остаток: " + myCardAcc.getBalance());
         myCardAcc.putMoney(10);
-        System.out.println(myCardAcc.getBalance());
+        System.out.println("Остаток: " + myCardAcc.getBalance());
         myCardAcc.withdrawMoney(50);
-        System.out.println(myCardAcc.getBalance());
-
+        System.out.println("Остаток: " + myCardAcc.getBalance());
     }
 }
--- Module6/Homework1/src/bank/CardAccount.java
+++ Module6/Homework1/src/bank/CardAccount.java
@@ -2,13 +2,17 @@ package bank;
 
 public class CardAccount extends Account {
 
-    private static double fee = 0.01;
+    private static double fee = 1;
 
     public CardAccount(double sum) {
         super(sum);
     }
 
     public void withdrawMoney(double sum) {
-        super.withdrawMoney(sum * (1 + fee));
+        super.withdrawMoney(sum * (1 + fee/100));
+    }
+
+    public static double getFee() {
+        return fee;
     }
 }
